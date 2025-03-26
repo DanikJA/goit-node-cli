ef2b220785af6397aa4789d8fb88c23837560e5f
Danik2023 <danik.kyrteu2@gmail.com>
2025-03-26 17:49:49 +0200

    ...

--- contacts.js
+++ contacts.js
@@ -2,3 +2,24 @@ const fs = require("fs/promises");
 const path = require("path");
 
 const contactsPath = path.join(__dirname, "contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading contacts:", error);
+  }
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
